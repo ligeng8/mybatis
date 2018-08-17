213c89f17e9880afab3bdd77771b7ca57aecb347
ligeng8 <36887805+ligeng8@users.noreply.github.com>
2018-08-17 10:15:34 +0800

    添加mybatis配置

--- springboot_mybatis_redis_shiro/src/main/java/com/dt/spring/config/MybatisConfig.java
+++ springboot_mybatis_redis_shiro/src/main/java/com/dt/spring/config/MybatisConfig.java
@@ -1,12 +1,60 @@
 package com.dt.spring.config;
 
-import javax.annotation.Resource;
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.mapper.MapperScannerConfigurer;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
+@AutoConfigureAfter(DataSourceConfig.class)
 public class MybatisConfig {
-    @Resource
-	private DataSource dataSource;
+
+	@Bean
+	public PlatformTransactionManager annotationDriverTransactionManager(DataSource dataSource) {
+		DataSourceTransactionManager dataSourceTransactionManager = new DataSourceTransactionManager(dataSource);
+		return dataSourceTransactionManager;
+	}
+
+	@Bean
+	public PersistenceExceptionTranslationPostProcessor persistenceExceptionTranslationPostProcessor() {
+		PersistenceExceptionTranslationPostProcessor postProcessor = new PersistenceExceptionTranslationPostProcessor();
+		return postProcessor;
+	}
+	
+    @Bean
+	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) {
+		SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+		sqlSessionFactoryBean.setDataSource(dataSource);
+		sqlSessionFactoryBean.setConfigLocation(new ClassPathResource("/sqlMapConfig.xml"));
+		try {
+			return sqlSessionFactoryBean.getObject();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+    
+    
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+    	return new SqlSessionTemplate(sqlSessionFactory);
+    }
+    
+    @Bean
+    public MapperScannerConfigurer scannerConfigurer(SqlSessionFactory sqlSessionFactory) {
+    	MapperScannerConfigurer scannerConfigurer = new MapperScannerConfigurer();
+    	scannerConfigurer.setSqlSessionFactoryBeanName("sqlSessionFactory");
+    	scannerConfigurer.setBasePackage("com.it.mpper");
+    	return scannerConfigurer ;
+    }
 }
